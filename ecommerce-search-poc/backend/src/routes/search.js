46687b72d637ba7ba0275207710fce53bc51fb29
deividdiasrodrigues <79477697+deividdiasrodrigues@users.noreply.github.com>
2026-03-11 18:30:37 -0300

    Delete ecommerce-search-poc directory

--- ecommerce-search-poc/backend/src/routes/search.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { search } = require("../controllers/searchController");
-
-const router = Router();
-
-router.get("/search", search);
-
-module.exports = router;
